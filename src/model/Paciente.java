6a253f9120d22072ae66563ea8048d36c6440392
REBECA GOMES DA SILVA <25132910@CFP127ALUNO.LOCAL>
2025-02-10 11:25:40 -0300

    Criação dos atributos e assinatura dos métodos na Classe Paciente.

--- src/model/Paciente.java
+++ src/model/Paciente.java
@@ -1,4 +1,23 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Paciente {
+
+    String nome;
+    double peso;
+    double altura;
+    String genero;
+    String Telefone;
+    LocalDate dataNascimento;
+
+    public void marcarConsulta(){
+
+    }
+
+    public void calcularIdade(){
+
+    }
+
+    public void
 }
